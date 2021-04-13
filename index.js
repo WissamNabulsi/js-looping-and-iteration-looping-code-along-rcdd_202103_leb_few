f206b3c34611616bbdff2c561dbfb378393d7a6b
Wissam Nabulsi <wissamnabulsi75@gmail.com>
2021-04-13 14:54:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Code your solutions in this file
-let names =['lisa' , 'Joe' , 'Sonya'];
-let events = 'surprise';
-
-function writeCards(names,events){
-  let messages=[];
- for (let i=0; i<names.length; i++) {
-messages.push('Thank you, ${names[i]}, for the amazing ${events} gift!;');
-}
-return messages;
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
 }
 
-console.log(writeCards(['lisa', 'Joe' , 'Sonya'], 'surprise'))
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}

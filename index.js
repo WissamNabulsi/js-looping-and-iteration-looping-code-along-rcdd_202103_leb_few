a475e65ce50228b993d3eedde706e000417c0b69
Wissam Nabulsi <wissamnabulsi75@gmail.com>
2021-04-13 14:49:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+let names =['lisa' , 'Joe' , 'Sonya'];
+let events = 'surprise';
+
+function writeCards(names,events){
+  let messages=[];
+ for (let i=0; i<names.length; i++) {
+messages.push('Thank you, ${names[i]}, for the amazing ${events} gift!;');
+}
+return messages;
+}
+
+console.log(writeCards(['lisa', 'Joe' , 'Sonya'], 'surprise'))
